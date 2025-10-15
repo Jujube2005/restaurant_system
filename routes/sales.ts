4a7c7b94a270212bbf2b2f54f6940eea53ec735e
jujubea <chotika.jakchai@gmail.com>
2025-10-15 16:30:58 +0700

    Update sales.ts

--- routes/sales.ts
+++ routes/sales.ts
@@ -1,6 +1,19 @@
 import express from "express";
-import { getSalesSummary } from "../controllers/salesController.js";
+import { db } from "../utils/db.js";
+import { authenticateToken, authorizeRoles } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
-router.post("/", getSalesSummary);
+
+router.get(
+  "/summary",
+  authenticateToken,        
+  authorizeRoles("admin"),  
+  async (req, res) => {
+    const [rows]: any = await db.query(
+      "SELECT SUM(total_price) AS totalSales FROM sales"
+    );
+    res.json(rows[0]);
+  }
+);
+
 export default router;
