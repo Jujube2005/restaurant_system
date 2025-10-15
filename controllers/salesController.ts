b125bef550368c13ead89a1d80436b5f567d65ea
jujubea <chotika.jakchai@gmail.com>
2025-10-15 15:16:20 +0700

    Update salesController.ts

--- controllers/salesController.ts
+++ controllers/salesController.ts
@@ -1,7 +1,7 @@
 import { type Request, type Response } from "express";
 import { db } from "../utils/db.js";
 
-async function getSalesSummary(req: Request, res: Response) {
+export async function getSalesSummary(req: Request, res: Response) {
     const [rows]: any = await db.query(
         "SELECT SUM(total_price) FROM orders;"
     );
